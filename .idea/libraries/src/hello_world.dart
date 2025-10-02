9546fdc924823fab4e0cca12bb6ac561a33b02e5
brian-mokaya <brianmokaya@gmail.com>
2025-10-02 10:12:33 +0300

    first commit

---
+++ .idea/libraries/src/hello_world.dart
@@ -0,0 +1,17 @@
+void main () {
+  print('Hello, World!');
+
+  String wizardName = "Mokaya";
+  int magicPower = 100;
+  double spellStrength = 75.5;
+  bool isEvil = false;
+
+  print("Wizard Name: $wizardName");
+  print("Magic Power: $magicPower");
+  print("Spell Strength: $spellStrength");
+  print("Is Evil: $isEvil");
+
+//   update wizard's magic power
+  magicPower += 50;
+  print("Updated Magic Power: $magicPower");
+}
